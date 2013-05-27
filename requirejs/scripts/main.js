dc01eba58149eba1bf0c001634fbd69ee1c1acd0
zhgjiang <unsafe_june@163.com>
2013-05-27 11:56:47 +0800

    jquery

--- requirejs/scripts/main.js
+++ requirejs/scripts/main.js
@@ -1,5 +1,19 @@
 (function(){
 
+	//jQuery
+	requirejs.config({
+		  //To get timely, correct error triggers in IE, force a define/shim exports check.
+		  enforceDefine: true,
+		  paths: {
+		      jquery: [
+		          "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min",
+		          //If the CDN location fails, load from this location
+		          "lib/jquery/jquery"
+		      ]
+		  }
+	});
+
+
 	//版本
 	require(["helper/util"], function(util) {
 	
@@ -18,6 +32,11 @@
 			namespace.namespace(global, "dom.crud");
 		  util.p(global);
 		  
+	});
+
+	//Later
+	require(["jquery"], function ($) {
+		$("body").append("<p>jquery append</p>");
 	});
 
 })();
